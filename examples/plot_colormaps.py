9956b3c908b114efbecb14512aa0b5c528112389
Plutoset <tangbo@stu.ecnu.edu.cn>
2024-04-15 18:07:24 +0800

    colormaps example remake

--- examples/plot_colormaps.py
+++ examples/plot_colormaps.py
@@ -4,6 +4,7 @@ import inspect
 
 import matplotlib.pyplot as plt
 import matplotlib
+
 matplotlib.rc('text', usetex=False)
 
 
@@ -18,15 +19,14 @@ if __name__ == '__main__':
     color = list_cmaps()
 
     a = np.outer(np.arange(0, 1, 0.001), np.ones(10))
-    plt.figure(figsize=(20, 20))
+    plt.figure(figsize=(40, 20))
     plt.subplots_adjust(top=0.95, bottom=0.05, left=0.01, right=0.99)
     ncmaps = len(color)
-    nrows = 8
+    ncols = 8
     for i, k in enumerate(color):
-        plt.subplot(nrows, ncmaps // nrows + 1, i + 1)
+        plt.subplot(ncmaps // ncols + 1, ncols * 2, 2 * (i + 1))
         plt.axis('off')
-        plt.imshow(a, aspect='auto', cmap=getattr(cmaps, k), origin='lower')
-        plt.title(k, rotation=90, fontsize=10)
-        plt.title(k, fontsize=10)
+        plt.imshow(a.T, aspect='auto', cmap=getattr(cmaps, k), origin='lower')
+        plt.text(0, 0, k, fontsize=10, ha='right', va='bottom')
     plt.savefig('colormaps.png', dpi=300)
     plt.close()
